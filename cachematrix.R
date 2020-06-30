d99d3a922b6b4f349a3318cd420787cec667d771
JOHNFTHORNTON <johnfthornton@yahoo.co.uk>
2020-06-30 21:55:02 +0100

    this was updating the core function framework to store the relevant matrices in the cache while calculating the inverse in the main cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function creates a special matrix
+## object thaat can cache its inverse
+## function assumes that an invertible matrix is passed to it
+
+## The cacheSolve function computes the inverse of the
+## special matrix returned by the makeCacheMatrix above
+## If he inverse has already been calculated and the matrix
+## is unchanged, then the cacheSolve should retrieve the inverse
+## from the cache
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y = matrix()) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Inverse) m <<- Inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setInverse(m)
+  m
+  
 }
